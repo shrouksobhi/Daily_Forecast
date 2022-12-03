db99a243726a2b205571bfacaaec22c403edd086
shrouksobhi <shrouksobhi99@gmail.com>
2022-12-03 18:21:06 +0200

    add bindingAdapter

---
+++ app/src/main/java/com/shrouk/dailyforecast/adapter/ForecastBindingAdapter.kt
@@ -0,0 +1,15 @@
+package com.shrouk.dailyforecast.adapter
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+//
+//@BindingAdapter("formatminTemp")
+//fun TextView.setMinTemp(minTemp:String){
+//
+//    this.text=minTemp.plus("℃")
+//}
+//@BindingAdapter("formatmaxTemp")
+//fun TextView.setMaxTemp(maxTemp:String){
+//
+//    this.text=maxTemp.plus("℃")
+//}
