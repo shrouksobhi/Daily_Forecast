6341f7f5a4cefdcef5fd72c453d865380048061e
shrouksobhi <shrouksobhi99@gmail.com>
2022-12-03 23:22:56 +0200

    update NetworkModule

--- app/src/main/java/com/shrouk/dailyforecast/di/NetworkModule.kt
+++ app/src/main/java/com/shrouk/dailyforecast/di/NetworkModule.kt
@@ -1,13 +1,14 @@
 package com.shrouk.dailyforecast.di
 
 
+import android.content.Context
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
-import com.shrouk.dailyforecast.App
 import com.shrouk.dailyforecast.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -19,24 +20,28 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
     private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    private val okHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(
-        ChuckerInterceptor.Builder(App.applicationContext())
-            .collector(ChuckerCollector(App.applicationContext()))
-            .maxContentLength(250000L)
-            .redactHeaders(emptySet())
-            .alwaysReadResponseBody(false)
-            .build()    ).build()
 
     @Provides
     @Singleton
-    fun provideRetrofit() : Retrofit =
+    fun provideHttpClient(@ApplicationContext applicationContext: Context): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(
+            ChuckerInterceptor.Builder(applicationContext)
+                .collector(ChuckerCollector(applicationContext))
+                .maxContentLength(250000L)
+                .redactHeaders(emptySet())
+                .alwaysReadResponseBody(false)
+                .build()
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)//"https://api.openweathermap.org/data/2.5/"
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
-
-
 
 
 }
