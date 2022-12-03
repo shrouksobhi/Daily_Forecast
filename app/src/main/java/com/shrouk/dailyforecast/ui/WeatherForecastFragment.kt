9c889baae77cfae34cf72af697658763a3705fee
shrouksobhi <shrouksobhi99@gmail.com>
2022-12-03 18:12:32 +0200

    WeatherForecastFragment

---
+++ app/src/main/java/com/shrouk/dailyforecast/ui/WeatherForecastFragment.kt
@@ -0,0 +1,140 @@
+package com.shrouk.dailyforecast.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.shrouk.dailyforecast.R
+import com.shrouk.dailyforecast.adapter.WeatherForecastAdapter
+import com.shrouk.dailyforecast.databinding.FragmentWeatherForecastBinding
+import com.shrouk.dailyforecast.forecastExt.makeGone
+import com.shrouk.dailyforecast.forecastExt.makeVisible
+import com.shrouk.dailyforecast.model.Cod
+import com.shrouk.dailyforecast.model.WeatherList
+import com.shrouk.dailyforecast.model.WeatherResponse
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WeatherForecastFragment : Fragment() {
+    private lateinit var binding: FragmentWeatherForecastBinding
+    private lateinit var weatherForecastAdapter: WeatherForecastAdapter
+    private val weatherForecastViewModel: WeatherForecastViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_weather_forecast, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = binding.recyclerview
+
+        binding.search.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                val query = binding.search.text.toString()
+                if (query.isEmpty()) {
+                    Toast.makeText(requireContext(), "Enter city name !", Toast.LENGTH_SHORT).show()
+                } else {
+                    weatherForecastViewModel.getCityForecast(query)
+                    observeResponse()
+                    try {
+
+                        val imm: InputMethodManager =
+                            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
+                    } catch (e: Exception) {
+                        Log.e("TAG", "${e.localizedMessage} ")
+                    }
+                }
+            }
+            return@setOnEditorActionListener false
+        }
+
+        binding.retry.setOnClickListener {
+            binding.bottomConstrain.makeGone()
+            val query = binding.search.text.toString()
+            weatherForecastViewModel.getCityForecast(query)
+        }
+    }
+
+    private fun observeResponse() {
+        binding.notAccurate.makeGone()
+
+        weatherForecastViewModel.cityforecast.observe(viewLifecycleOwner) {
+            when (it.cod) {
+                Cod.OK -> {
+                    binding.recyclerview.makeVisible()
+                    binding.notAccurate.makeGone()
+
+                    val datalist = it.weatherlist
+                    installviews(datalist!!)
+                    val weatherlist = it.weatherlist
+                    val weathercity = it.weathercity
+                    val weatherResponse = WeatherResponse(weatherlist, weathercity!!)
+                    weatherForecastViewModel.saveForecast(weatherResponse)
+
+
+                }
+                Cod.ERROR -> {
+                    binding.recyclerview.makeVisible()
+                    val searchedCity = binding.search.text.toString()
+                    weatherForecastViewModel.getSavedForecast(searchedCity)
+                    observeRoomResponse()
+                    // Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+                Cod.LOADING ->
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.city_forecast_loading_state),
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+            }
+        }
+    }
+
+    private fun observeRoomResponse() {
+        //  weatherForecastViewModel.city.remove()
+        weatherForecastViewModel.city.observe(viewLifecycleOwner) {
+
+            val list = (it.weatherList)
+            //   Log.e("TAG", "observeRoomResponse: $list")
+            if (list.isNotEmpty()) {
+                binding.notAccurate.makeVisible()
+                binding.bottomConstrain.makeGone()
+                binding.recyclerview.makeVisible()
+                installviews(java.util.ArrayList(list))
+            } else {
+                binding.notAccurate.makeGone()
+                binding.recyclerview.makeGone()
+                binding.bottomConstrain.makeVisible()
+            }
+        }
+    }
+
+    private fun installviews(weatherlist: ArrayList<WeatherList>) {
+        val layoutManager: LayoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = layoutManager
+        weatherForecastAdapter = WeatherForecastAdapter(weatherlist)
+        recyclerView.adapter = weatherForecastAdapter
+
+    }
+}
