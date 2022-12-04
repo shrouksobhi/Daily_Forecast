8e6e3ab4c14617201a3b4aa5ea34b79c293a32a3
shrouksobhi <shrouksobhi99@gmail.com>
2022-12-04 03:12:56 +0200

    HiltTestActivity

---
+++ app/src/debug/java/com/shrouk/dailyforecast/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.shrouk.dailyforecast
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity :AppCompatActivity()
